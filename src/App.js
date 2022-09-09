2f5e487ad6321de699d516f243b764421d369b1c
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-09 06:19:40 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import './App.css';
+import { useState } from 'react';
+import Sidebar from './components/SideBar';
+import Editor from './components/Editor';
+import { Data } from './Data';
+import Split from 'react-split';
+import { nanoid } from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
+import './styles.css';
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ''
   );
-}
+
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  };
+
+  const updateNote = (text) => {
+    setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote;
+      })
+    );
+  };
+
+  const findCurrentNote = () => {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  };
+
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction='horizontal' className='split'>
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className='no-notes'>
+          <h1>You have no notes</h1>
+          <button className='first-note' onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default App;
